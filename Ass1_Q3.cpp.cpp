098e01ec5395bee8fa02be40771b5a1b2b8c1c29
shikhar173 <147432490+shikhar173@users.noreply.github.com>
2026-08-17 12:03:17 +0530

    Add files via upload

---
+++ Ass1_Q3.cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   // for(int i = 0;;i++){
+      //  cout << i;
+    //} 
+    
+    cout << endl;
+    
+    int i = 0;
+    for(;i<10;i++){
+        cout << i;
+    }
+    
+    return 0;
+}
